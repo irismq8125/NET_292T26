ac5803eb7791a807e58bd863b265498ce127f22f
hv <hv@PM309-GV>
2024-03-15 18:08:21 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_292T26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Nhap vao so thang = ");
+            //int i = int.Parse(Console.ReadLine());
+
+            //if(i % 3 == 0)
+            //{
+            //    Console.WriteLine("So chia het cho 3");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("So khong chia het cho 3");
+            //}
+
+            //if (i % 3 == 0) Console.WriteLine("So chia het cho 3");
+            //else Console.WriteLine("So khong chia het cho 3");
+
+            //Console.WriteLine(i % 3 == 0 ? "So chia het cho 3" : "So khong chia het cho 3"); // ? = if, : = else
+
+            //string kq = "So chia het cho 3";
+            //if (i % 3 != 0) kq = "So khong chia het cho 3";
+
+            //Console.WriteLine(kq);
+
+
+            //switch (i)
+            //{
+            //    case 1: case 3:
+            //    case 5:
+            //    case 7:
+            //        Console.WriteLine("Co 31 ngay");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Co 28 hoac 29 ngay");
+            //        break;
+            //    case 4: case 6:
+            //        Console.WriteLine("Co 30 ngay");
+            //        break;
+            //}
+            //Console.WriteLine("1");
+            //Console.WriteLine("2");
+            //Console.WriteLine("3");
+            //Console.WriteLine("4");
+            //Console.WriteLine("5");
+            //Console.WriteLine("6");
+            //Console.WriteLine("7");
+            //Console.WriteLine("8");
+            //Console.WriteLine("9");
+            //Console.WriteLine("10");
+            //Console.WriteLine("---------------");
+
+            //int tong = 0;
+            int j = 1;
+            if (j % 2 != 0) break;
+            Console.WriteLine(j);
+            //for (int j = 1; j <= 10; j ++) // j = j + 1 | j++
+            //{
+
+            //}
+
+
+
+            //Console.WriteLine(tong);
+
+            Console.ReadKey();
+        }
+    }
+}
